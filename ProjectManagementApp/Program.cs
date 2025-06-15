0ef8a8fd7939efea857e7d6e4d99e827d9be5783
s1727780 <Mike.chester99@outlook.com>
2025-06-15 22:36:27 +0100

    C,R,D implemented

--- ProjectManagementApp/Program.cs
+++ ProjectManagementApp/Program.cs
@@ -1,9 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Http.HttpResults;
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Host configuration goes here
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Task> tasks = new List<Task>();
+
+app.MapGet("/tasks", () => tasks);
+
+app.MapGet("/tasks/{id}", Results<Ok<Task>, NotFound<int>> (int id) =>
+{
+    Task? targetTask = tasks.SingleOrDefault(t => id == t.id);
+
+    if (targetTask == null)
+    {
+        return TypedResults.NotFound(id);
+    }
+
+    return TypedResults.Ok(targetTask);
+
+});
+
+
+app.MapPost("/tasks", (Task task) =>
+{
+    tasks.Add(task);
+    return TypedResults.Created("/tasks/{id}", task);
+});
+
+app.MapDelete("/tasks/{id}", (int id) =>
+{
+    tasks.RemoveAll(t => id == t.id);
+    return TypedResults.NoContent();
+});
 
 app.Run();
+
+public record Task(int id, string Name, DateTime DueDate, bool isCompleted);
