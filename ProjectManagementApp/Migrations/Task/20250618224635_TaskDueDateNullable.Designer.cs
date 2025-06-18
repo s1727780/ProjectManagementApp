8781717934aa81e44dbd72045bdc7969f7287cc6
s1727780 <Mike.chester99@outlook.com>
2025-06-18 23:48:46 +0100

    SQL - Due Date Nullable

--- ProjectManagementApp/Migrations/Task/20250618224635_TaskDueDateNullable.Designer.cs
+++ ProjectManagementApp/Migrations/Task/20250618224635_TaskDueDateNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagementApp.Model;
 
@@ -11,9 +12,11 @@ using ProjectManagementApp.Model;
 namespace ProjectManagementApp.Migrations.Task
 {
     [DbContext(typeof(TaskContext))]
-    partial class TaskContextModelSnapshot : ModelSnapshot
+    [Migration("20250618224635_TaskDueDateNullable")]
+    partial class TaskDueDateNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
