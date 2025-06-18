10d7fa2243d59169580091c94640f001279fc231
s1727780 <Mike.chester99@outlook.com>
2025-06-18 23:19:50 +0100

    Created project model

---
+++ ProjectManagementApp/Model/Project.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManagementApp.Model
+{
+    public class Project : DbContext{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
