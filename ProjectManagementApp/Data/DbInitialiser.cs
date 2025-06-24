7ce49b501ea73d6d28dd61c1a4b8b997bde6c87b
s1727780 <mike.chester99@outlook.com>
2025-06-24 16:07:23 +0100

    Added craete db method

--- ProjectManagementApp/Data/DbInitialiser.cs
+++ ProjectManagementApp/Data/DbInitialiser.cs
@@ -35,5 +35,18 @@ namespace ProjectManagementApp.Data {
 
         }
 
+        public static void CreateDbIfNotExists(IHost host) {
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                try {
+                    var context = services.GetRequiredService<TaskContext>();
+                    Initialise(context);
+                } catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
     }
 }
